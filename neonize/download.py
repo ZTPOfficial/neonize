171da2e209a996f96d74441ea67ace67fd921b6e
zhann <zhanprasetyaningrat@gmail.com>
2025-11-14 22:46:30 +0700

    fix

--- neonize/download.py
+++ neonize/download.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 
 from .utils.platform import generated_name
 
-__GONEONIZE_VERSION__ = "0.3.12.5"
+__GONEONIZE_VERSION__ = "0.3.13.5"
 __GIT_RELEASE_URL__ = "https://github.com/ZTPOfficial/neonize"
 
 
