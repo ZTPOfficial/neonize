4cc4b1b6138fbff741d569a6f69afc8eb64383a5
zhann <zhanprasetyaningrat@gmail.com>
2025-11-14 22:12:52 +0700

    Add bypass and generatemessageid

--- neonize/proto/waStatusAttributions/WAStatusAttributions_pb2.pyi
+++ neonize/proto/waStatusAttributions/WAStatusAttributions_pb2.pyi
@@ -35,6 +35,7 @@ class StatusAttribution(google.protobuf.message.Message):
         GROUP_STATUS: StatusAttribution._Type.ValueType  # 5
         RL_ATTRIBUTION: StatusAttribution._Type.ValueType  # 6
         AI_CREATED: StatusAttribution._Type.ValueType  # 7
+        LAYOUTS: StatusAttribution._Type.ValueType  # 8
 
     class Type(_Type, metaclass=_TypeEnumTypeWrapper): ...
     UNKNOWN: StatusAttribution.Type.ValueType  # 0
@@ -45,6 +46,7 @@ class StatusAttribution(google.protobuf.message.Message):
     GROUP_STATUS: StatusAttribution.Type.ValueType  # 5
     RL_ATTRIBUTION: StatusAttribution.Type.ValueType  # 6
     AI_CREATED: StatusAttribution.Type.ValueType  # 7
+    LAYOUTS: StatusAttribution.Type.ValueType  # 8
 
     @typing.final
     class AiCreatedAttribution(google.protobuf.message.Message):
@@ -124,6 +126,7 @@ class StatusAttribution(google.protobuf.message.Message):
             THREADS: StatusAttribution.ExternalShare._Source.ValueType  # 7
             APPLE_MUSIC: StatusAttribution.ExternalShare._Source.ValueType  # 8
             SHARECHAT: StatusAttribution.ExternalShare._Source.ValueType  # 9
+            GOOGLE_PHOTOS: StatusAttribution.ExternalShare._Source.ValueType  # 10
 
         class Source(_Source, metaclass=_SourceEnumTypeWrapper): ...
         UNKNOWN: StatusAttribution.ExternalShare.Source.ValueType  # 0
@@ -136,6 +139,7 @@ class StatusAttribution(google.protobuf.message.Message):
         THREADS: StatusAttribution.ExternalShare.Source.ValueType  # 7
         APPLE_MUSIC: StatusAttribution.ExternalShare.Source.ValueType  # 8
         SHARECHAT: StatusAttribution.ExternalShare.Source.ValueType  # 9
+        GOOGLE_PHOTOS: StatusAttribution.ExternalShare.Source.ValueType  # 10
 
         ACTIONURL_FIELD_NUMBER: builtins.int
         SOURCE_FIELD_NUMBER: builtins.int
